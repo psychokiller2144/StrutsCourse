0798ac0585a8875fd039075e1d71373ff040f9f3
psychokiller2144 <79776630+psychokiller2144@users.noreply.github.com>
2022-07-13 06:18:32 -0500

    Add files via upload

---
+++ Lesson6/AnotationsStruts/src/main/java/beans/actions/ValidateUserAction.java
@@ -0,0 +1,47 @@
+package beans.actions;
+
+import static com.opensymphony.xwork2.Action.INPUT;
+import static com.opensymphony.xwork2.Action.SUCCESS;
+import com.opensymphony.xwork2.ActionSupport;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.struts2.convention.annotation.Action;
+import org.apache.struts2.convention.annotation.Result;
+import org.apache.struts2.convention.annotation.Results;
+
+@Results({
+    @Result(name="success", location="/WEB-INF/content/welcome.jsp"),
+    @Result(name="input", location="login",type="redirectAction")
+       
+})
+public class ValidateUserAction extends ActionSupport {
+    Logger log = LogManager.getLogger(LoginAction.class);
+    
+    private String usuario;
+    private String password;
+    
+    @Action("ValidateUser")
+    public String execute(){
+        if("admin".equals(this.usuario)){
+            return SUCCESS;
+        }else{
+            return INPUT;
+        }
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
---
+++ Lesson6/AnotationsStruts/target/AnotationsStruts-1.0/WEB-INF/content/welcome.jsp
@@ -0,0 +1,18 @@
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<%@taglib prefix="s" uri="/struts-tags" %>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title><s:text name="welcome.title"/></title>
+    </head>
+    <body>
+        <h1>¡<s:text name="welcome.title"/>!</h1>
+        <h2> <s:text name="welcome.message"/> </h2>
+        
+        <p> 
+            <s:text name="form.usuario"/>: <s:property value="usuario"/> <br>
+            <s:text name="form.password"/>: <s:property value="password"/> <br>
+        </p>
+        <a href="<s:url action="login"/>">  <s:text name="welcome.return"/> </a>
+    </body>
+</html>
